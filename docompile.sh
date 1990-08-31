75727aa3f57c228f4588860adb081aad271cc88a
layer <layer>
1990-08-31 23:48:46 +0000

    make sure $3 looks like a directory.  Require :defctype instead of :cstructs (the wave of the future).

--- docompile.sh
+++ docompile.sh
@@ -1,19 +1,19 @@
-#! /bin/sh -ex
-# $Header: /repo/cvs.copy/eli/Attic/docompile.sh,v 1.5 1989/08/16 13:51:59 layer Rel $
+#! /bin/sh -x
+# $Header: /repo/cvs.copy/eli/Attic/docompile.sh,v 1.6 1990/08/31 23:48:46 layer Exp $
 
 # usage: $0 file cl_binary [cl_library]
 
 if test -z "$3"; then
 	libdir="excl::*library-code-pathname*"
 else
-	libdir="\"$3\""
+	libdir="\"$3/\""
 fi
 
 $2 -qq -batch << EOF
 (set-case-mode :case-sensitive-lower)
 (require :process)
 (require :foreign)
-(require :cstructs)
+(require :defctype)
 (compile-file "$1.cl" :output-file (namestring
 				     (merge-pathnames "$1.fasl" $libdir)))
 EOF
