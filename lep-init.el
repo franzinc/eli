c8b3f564712da40d8f01a090141ffbf627631d38
layer <layer>
1991-02-12 17:18:17 +0000

    load lze.elc

--- lep-init.el
+++ lep-init.el
@@ -1,9 +1,10 @@
-;; $Header: /repo/cvs.copy/eli/Attic/lep-init.el,v 1.2 1991/01/29 17:19:55 layer Exp $
+;; $Header: /repo/cvs.copy/eli/Attic/lep-init.el,v 1.3 1991/02/12 17:18:17 layer Exp $
 
 (load "fi/lep/basic-lep.elc")
 (load "fi/lep/lep.elc")
 (load "fi/lep/dmode.elc")
 (load "fi/lep/stream.elc")
 (load "fi/lep/composer.elc")
+(load "fi/lep/lze.elc")
 
 (setq fi::lep-loaded t)
