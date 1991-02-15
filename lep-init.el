aa83963cef6a2f278ea9527c0fdb3d8f77e314d8
layer <layer>
1991-02-15 23:22:07 +0000

    load db.elc

--- lep-init.el
+++ lep-init.el
@@ -1,4 +1,4 @@
-;; $Header: /repo/cvs.copy/eli/Attic/lep-init.el,v 1.3 1991/02/12 17:18:17 layer Exp $
+;; $Header: /repo/cvs.copy/eli/Attic/lep-init.el,v 1.4 1991/02/15 23:22:07 layer Exp $
 
 (load "fi/lep/basic-lep.elc")
 (load "fi/lep/lep.elc")
@@ -6,5 +6,6 @@
 (load "fi/lep/stream.elc")
 (load "fi/lep/composer.elc")
 (load "fi/lep/lze.elc")
+(load "fi/lep/db.elc")
 
 (setq fi::lep-loaded t)
