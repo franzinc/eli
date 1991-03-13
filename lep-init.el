bd56d4c40711258505562c42eeb63055dca1446f
layer <layer>
1991-03-13 15:03:12 +0000

    new file lep/changes.el

--- lep-init.el
+++ lep-init.el
@@ -1,4 +1,4 @@
-;; $Header: /repo/cvs.copy/eli/Attic/lep-init.el,v 1.4 1991/02/15 23:22:07 layer Exp $
+;; $Header: /repo/cvs.copy/eli/Attic/lep-init.el,v 1.5 1991/03/13 15:03:12 layer Exp $
 
 (load "fi/lep/basic-lep.elc")
 (load "fi/lep/lep.elc")
@@ -6,6 +6,7 @@
 (load "fi/lep/stream.elc")
 (load "fi/lep/composer.elc")
 (load "fi/lep/lze.elc")
+(load "fi/lep/changes.elc")
 (load "fi/lep/db.elc")
 
 (setq fi::lep-loaded t)
