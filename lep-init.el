a3d768bce848e122eab403ef41d065cf360db93b
layer <layer>
1991-03-28 12:57:13 +0000

    stream is history

--- lep-init.el
+++ lep-init.el
@@ -1,9 +1,8 @@
-;; $Header: /repo/cvs.copy/eli/Attic/lep-init.el,v 1.5 1991/03/13 15:03:12 layer Exp $
+;; $Header: /repo/cvs.copy/eli/Attic/lep-init.el,v 1.6 1991/03/28 12:57:13 layer Exp $
 
 (load "fi/lep/basic-lep.elc")
 (load "fi/lep/lep.elc")
 (load "fi/lep/dmode.elc")
-(load "fi/lep/stream.elc")
 (load "fi/lep/composer.elc")
 (load "fi/lep/lze.elc")
 (load "fi/lep/changes.elc")
