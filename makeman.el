e96516e11dfee39d8a5f952b34fdf52bbdb011cf
layer <layer>
1993-07-27 20:17:19 +0000

    add makeman.el

---
+++ makeman.el
@@ -0,0 +1,12 @@
+
+(require 'cl)
+
+(do ((ll fi::clman-big-oblist (cdr ll))
+     xx)
+    ((null ll) (kill-emacs 0))
+  (setq xx (car ll))
+  (unless (string-match ".*:" (car xx))
+    (message "%s" (car xx))
+    (dolist (x (cdr xx))
+      (message "%s" x))
+    (message "")))
