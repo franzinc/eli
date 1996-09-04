6120c516c2f8bd2e8ef7a1f8eeeb6a3324c48ddd
layer <layer>
1996-09-04 00:03:06 +0000

    see changelog for Tue Sep 03 16:54

--- make.bat
+++ make.bat
@@ -1,3 +1,25 @@
-@rem $Id: make.bat,v 2.2 1996/08/30 21:32:06 layer Exp $
+@rem $Id: make.bat,v 2.3 1996/09/04 00:03:05 layer Exp $
 @echo off
-nmake -nologo -f makefile.win32 %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+if exist c:\emacs-19.34\bin\runemacs.exe goto set1
+if exist d:\emacs-19.34\bin\runemacs.exe goto set2
+
+goto cannotfind
+
+:set1
+set emacs_dir=c:\emacs-19.34
+goto donmake
+
+:set2
+set emacs_dir=d:\emacs-19.34
+goto donmake
+
+:donmake
+nmake -nologo -f makefile.win32 emacs_dir=%emacs_dir% %1 %2 %3 %4 %5 %6 %7 %8 %9
+goto exit
+
+:cannotfind
+echo Cannot find emacs directory.
+goto exit
+
+:exit
