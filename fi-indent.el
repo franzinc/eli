777d2249a1b3373ed868a7ebc07f8146beab8b2b
layer <layer>
2006-12-01 22:30:02 +0000

    2006-12-01  Kevin Layer  <layer@gemini.franz.com>

--- fi-indent.el
+++ fi-indent.el
@@ -20,7 +20,7 @@
 ;; file named COPYING.  Among other things, the copyright notice
 ;; and this notice must be preserved on all copies.
 
-;; $Id: fi-indent.el,v 3.1 2005/12/01 21:55:05 layer Exp $
+;; $Id: fi-indent.el,v 3.1.2.1 2006/12/01 22:30:02 layer Exp $
 
 (defvar fi:lisp-electric-semicolon nil
   "*If non-nil, semicolons that begin comments are indented as they are
@@ -1779,8 +1779,9 @@ if matched at the beginning of a line, means don't indent that line."
   (put 'for-each-scratch-list tag '(like dolist))
   
   ;; AllegroCache
-  (put 'doclass tag '(like let))
-  (put 'doclass* tag '(like let))
+  (put 'doclass tag '(like dolist))
+  (put 'doclass* tag '(like dolist))
+  (put 'doset tag '(like dolist))
   )
 
 (let ((tag 'fi:franz-lisp-indent-hook))
