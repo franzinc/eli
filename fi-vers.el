ed985aebb18d9092e8163048d852e147662b804c
layer <layer>
2007-03-02 13:52:53 +0000

    update version

--- fi-vers.el
+++ fi-vers.el
@@ -1,5 +1,5 @@
 ;; automatically generate file--do not edit.
 (defvar fi:emacs-lisp-interface-version)
-(setq fi:emacs-lisp-interface-version "3.17")
+(setq fi:emacs-lisp-interface-version "3.19")
 (defvar fi::compiled-with-version)
 (setq fi::compiled-with-version (eval-when-compile (cons emacs-major-version emacs-minor-version)))
