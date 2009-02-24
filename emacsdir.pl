4acbfc9de797e6e225baa9e8a506202a5e827385
layer <layer>
2009-02-24 21:11:39 +0000

    2009-02-24    <layer@HOBART>

--- emacsdir.pl
+++ emacsdir.pl
@@ -1,14 +1,15 @@
-# $Id: emacsdir.pl,v 3.1 2005/11/11 19:54:57 layer Exp $
+# $Id: emacsdir.pl,v 3.1.42.1 2009/02/24 21:11:39 layer Exp $
 #
 # This script requires Cygwin Perl
 
 use Win32::Registry;
 
-Win32::Registry::RegOpenKeyEx(&HKEY_LOCAL_MACHINE, 'SOFTWARE\GNU\Emacs',
+Win32::Registry::RegOpenKeyEx(&HKEY_LOCAL_MACHINE,
+			      'SOFTWARE\Franz Inc.\GNU Emacs Installer',
 		0, &KEY_READ, $EmacsKey) ||
 	die "Couldn't retrieve emacs_dir from Registry!!\n";
 
-Win32::Registry::RegQueryValueEx($EmacsKey, 'emacs_dir', 0, $type, $emacsdir);
+Win32::Registry::RegQueryValueEx($EmacsKey, 'Install_Dir', 0, $type, $emacsdir);
 
 Win32::Registry::RegCloseKey($EmacsKey);
 
