40a63b0a3d7a2aac3144cc14c1099c656ee221a8
Mikel Bancroft <mikel@franz.com>
2009-07-02 12:29:19 -0700

    fix paren mismatch problem in eli fix.

--- fi-basic-lep.el
+++ fi-basic-lep.el
@@ -256,7 +256,7 @@ release.")
       (set-process-buffer process buffer))
     (when coding-system 
       ;; bug12456 -mikel
-      (fi:set-process-coding process coding-system)))
+      (fi:set-process-coding process coding-system))
     (set-process-filter process 'fi::lep-connection-filter)
     ;; new stuff to indicate that we want the lisp editor protocol
     (process-send-string process ":lep\n")
